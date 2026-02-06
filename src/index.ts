6593937fa6fbaca588566fa51f036cd8f68a5733
nonafox <admin@jwhgzs.com>
2026-02-06 22:07:57 +0800

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app: Application = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server')
+  res.send('Welcome to Stupig!!!')
 })
 
 app.listen(port, () => {
